0dcdeaff766f0b0b7c0e84f340e24537a4896977
Brendan Wilson <wils0692@algonquinlive.com>
2016-02-08 12:48:09 -0500

    feat:(all) fonder dump

---
+++ js/push-menu.js
@@ -0,0 +1,36 @@
+// Needs latest version of jQuery to run
+
+//$(function() {
+//  $('body').removeClass('noscript');
+//
+//  $('.toggle-btn, .close-btn').click(function() {
+//    toggleNav();
+//  });
+//});
+//
+//function toggleNav() {
+//  if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+//    // Display Nav when closed
+//    $('.site-wrapper').attr('data-state', 'slide-open');
+//  } else {
+//    // Hide Nav when open
+//    $('.site-wrapper').attr('data-state', 'slide-closed');
+//  }
+//}
+
+$(function(){
+    $('body').removeClass('noscript');
+    $('.toggle-btn, .close-btn').click(function(){
+    toggleNav();
+  });
+}); 
+    
+
+function toggleNav(){
+    if($('.site-wrapper').attr('data-state') == 'slide-closed'){
+        
+        $('.site-wrapper').attr('data-state', 'slide-open');
+    }else{
+        $('.site-wrapper').attr('data-state', 'slide-closed');
+    }
+}
